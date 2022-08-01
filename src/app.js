72be210ba1a2b74c638ee7bea04cb5f5fbc33386
govrial <36007441+govrial@users.noreply.github.com>
2022-08-01 23:51:51 +0300

    #4 app

--- src/app.js
+++ src/app.js
@@ -89,41 +89,30 @@ var swiper = new Swiper(".swiper", {
 
 ///////////////////////////////anchors
 
-///////////////////////////////////////
+///////////////////////////////////////hide header
 
 document.addEventListener('DOMContentLoaded', () => { // DOM готов к взаимодейтсвию
-
   const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
-
     const header = document.querySelector('.header') // находим header и записываем в константу
-
     let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
     let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
-
     window.addEventListener('scroll', () => { // при прокрутке страницы
-
       currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
-
       const headerHidden = () => header.classList.contains('header_hidden') // узнаем скрыт header или нет
-
       if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
         header.classList.add('header_hidden') // то скрываем header
       }
       if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
         header.classList.remove('header_hidden') // то отображаем header
       }
-
       prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
-
     })
-
   }
-
   onScrollHeader() // вызываем основную функцию onScrollHeader
-
 });
+/////////////////////////////////////////
 
-
+////////////////modal
 let modal = document.querySelector("#modal"),
   modalOverlay = document.querySelector("#modal-overlay"),
   closeButton = document.querySelector("#close-button"),
@@ -147,3 +136,4 @@ exitButton.addEventListener('click', close);
 openButtonAuth.addEventListener('click', open);
 openButtonReg.addEventListener('click', open);
 
+////////////////modal
